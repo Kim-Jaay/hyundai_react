7e1bfbbc584e281a4af0ee210731812dfb3eb393
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-09-13 12:42:25 +0900

    0913

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Header from './component/Header'
+
+const App = () => {
+
+  return (
+    <div className="Wrap">
+
+      <Header />
+    </div>
+  );
+}
+
+export default App;
