208e4431ff0a3f71b5ce6a68bb6e19f8ee9e6e82
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-09-19 15:37:44 +0900

    0919

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Solution from './component/Solution';
 import Promotion from './component/Promotion';
 import News from './component/News';
 import Footer from './component/Footer';
+import Totop from './component/Totop';
+import Popup from './component/Popup';
 
 const App = () => {
   return (
@@ -20,6 +22,8 @@ const App = () => {
         <Promotion />
         <News />
         <Footer />
+        <Totop />
+        <Popup />
       </main>
     </div>
   );
